3fe4836572d77d534b44310b979669bea5076159
hoaxoan <nguyendo2008@gmail.com>
2018-01-06 22:40:18 +0700

    - add source code.

--- Libraries/Nop.Data/Mapping/Common/APIClientsMap.cs
+++ Libraries/Nop.Data/Mapping/Common/APIClientsMap.cs
@@ -2,16 +2,20 @@ using Nop.Core.Domain.Common;
 
 namespace Nop.Data.Mapping.Catalog
 {
-    public partial class APIClientsMap : NopEntityTypeConfiguration<APIClients>
+    public partial class APIClientsMap
     {
-        public APIClientsMap()
-        {
-            this.ToTable("API_Clients");
-            this.HasKey(pam => pam.Id);
 
-            this.Property(pav => pav.ClientId).IsRequired();
-            this.Property(pav => pav.ClientSecret).IsRequired();
-            this.Property(pav => pav.CallbackUrl).IsRequired();
-        }
     }
+    //public partial class APIClientsMap : NopEntityTypeConfiguration<APIClients>
+    //{
+    //    public APIClientsMap()
+    //    {
+    //        this.ToTable("API_Clients");
+    //        this.HasKey(pam => pam.Id);
+
+    //        this.Property(pav => pav.ClientId).IsRequired();
+    //        this.Property(pav => pav.ClientSecret).IsRequired();
+    //        this.Property(pav => pav.CallbackUrl).IsRequired();
+    //    }
+    //}
 }
